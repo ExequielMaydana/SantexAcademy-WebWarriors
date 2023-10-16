78b4ce8f1cf04991bfe13a167d1f7c4d04c8b3d3
ExequielMaydana <exedevcoding22@gmail.com>
2023-10-16 16:36:41 -0300

    get volunteerings page organization

--- front/src/app/pages/organization-details/organization-details.component.ts
+++ front/src/app/pages/organization-details/organization-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrgServicesService } from 'src/app/services/org-services.service';
 import { OrganizationService } from '../../../app/modules/dashboard/services/organization.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { volunteering } from '../../../app/modules/dashboard/models/volunteerings.model';
 import { selectToken } from '../../core/auth.selectors';
@@ -11,53 +11,43 @@ import { selectToken } from '../../core/auth.selectors';
   styleUrls: ['./organization-details.component.css'],
 })
 export class OrganizationDetailsComponent implements OnInit {
-
   constructor(
     private route: ActivatedRoute,
     private orgS: OrgServicesService,
-    private store: Store, private orgServices: OrganizationService
+    private store: Store,
+    private orgServices: OrganizationService,
+    private router: Router
   ) {}
   organizationId: number = 0;
   organization: any = {};
   volunteerings: volunteering[] = [];
 
   ngOnInit(): void {
-
     this.route.queryParams.subscribe((queryParams) => {
       this.organizationId = +queryParams['id'];
       this.orgS.getOrganizationById(this.organizationId.toString()).subscribe({
         next: (response) => {
-          console.log(response);
-
           this.organization = response;
-          this.getAllMeVolunteerings();
         },
         error: (error) => {
           console.error('Error in bringing the organization', error);
         },
         complete: () => {},
       });
+
+      this.orgServices.volsByIdOrg(this.organizationId.toString()).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.volunteerings = res;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
     });
   }
 
-  getAllMeVolunteerings() {
-    this.store.select(selectToken).subscribe((token) => {
-      if (token) {
-        this.orgServices.volunteeringByIdOrg(token).subscribe({
-          next: (res) => {
-            this.volunteerings = res;
-          },
-          error: (err) => {
-            console.log(err);
-          },
-        });
-      }
-    });
+  viewVolunteering(id: any) {
+    this.router.navigateByUrl(`/voluntariado/${id}`);
   }
-
-
-
-
-
-
 }
